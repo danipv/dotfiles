288e524a3cccc9bffb460fac1fe7bda0b252dd64
Daniel Pascual <daniel.s.pascual@gmail.com>
2013-07-23 17:57:35 +0000

    Add orgmodeconfig, gitignore with emacs temp files

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -77,6 +77,10 @@
 (add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
 
 ;; ----------------------
-;; -- Spanish Keyboard --
+;; ------ Org Mode ------
 ;; ----------------------
-(set-keyboard-coding-system 'utf-8)
+(require 'org-install)
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
