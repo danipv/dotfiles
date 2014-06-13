a10c738541a61e90587562db4586d2e47534d598
Daniel Pascual <daniel.pascual@eburypartners.com>
2014-06-13 16:51:37 +0200

    jedi and solarize

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -91,3 +91,24 @@
 (setq-default indent-tabs-mode nil)
 
 (global-set-key (kbd "C-x j") 'python-django-open-project)
+
+
+;; Standard package.el + MELPA setup
+;; (See also: https://github.com/milkypostman/melpa#usage)
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+
+;; Standard Jedi.el setting
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:complete-on-dot t)
+
+;; Type:
+;;     M-x package-install RET jedi RET
+;;     M-x jedi:install-server RET
+;; Then open Python file.
+
+
+(load-theme 'solarized-dark t)
